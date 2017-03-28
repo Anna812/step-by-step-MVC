9f1cf55821f538c2581040ef2b1f146545ea1cb8
Anna812 <88menyus@gmail.com>
2017-03-28 17:08:05 +0200

    create EmployeeViewModel class

---
+++ stepByStepMVC/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace stepByStepMVC.ViewModels
+{
+    public class EmployeeViewModel
+    {
+    }
+}
