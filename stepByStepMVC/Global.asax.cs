45b6078958c4bccee5f98968e7408db48ef5cfb7
Anna812 <88menyus@gmail.com>
2017-03-30 13:46:27 +0200

    set initializer for dropping table if model changes

--- stepByStepMVC/Global.asax.cs
+++ stepByStepMVC/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using stepByStepMVC.DataAccessLayer;
 
 namespace stepByStepMVC
 {
@@ -16,6 +18,7 @@ namespace stepByStepMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SalesERPDataAccessLayer>());
         }
     }
 }
